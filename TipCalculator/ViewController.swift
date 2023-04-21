06a1c8f09a43a5f09c21fd91fbcaeaff7b1dcaf2
Alexandre Barde <alexandre@brde.fr>
2023-04-21 22:41:23 +0200

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Alexandre Barde on 21/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
